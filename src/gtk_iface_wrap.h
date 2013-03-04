b94eb9d109e5ba5436fddbf212b0459b04618095
despierto <eterno.despierto@gmail.com>
2013-03-04 07:33:51 +0200

    Added definition of x event types. Add ico.

--- src/gtk_iface_wrap.h
+++ src/gtk_iface_wrap.h
@@ -58,6 +58,32 @@ typedef GtkWindowType   GTK_WINDOW_TYPE,   *PGTK_WINDOW_TYPE;
 #define GTK_EVENT_TYPE_DELETE       "delete_event"
 #define GTK_EVENT_TYPE_DESTROY      "destroy"
 
+#define GTK_EVENT_TYPE_EVENT            "event"
+#define GTK_EVENT_TYPE_BUTTON_PRESS     "button_press_event"
+#define GTK_EVENT_TYPE_BUTTON_RELEASE   "button_release_event"
+#define GTK_EVENT_TYPE_SCROLL           "scroll_event"
+#define GTK_EVENT_TYPE_MOTION_NOTIFY    "motion_notify_event"
+#define GTK_EVENT_TYPE_EXPOSE           "expose_event"
+#define GTK_EVENT_TYPE_KEY_PRESS        "key_press_event"
+#define GTK_EVENT_TYPE_KEY_RELEASE      "key_release_event"
+#define GTK_EVENT_TYPE_ENTER_NOTIFY     "enter_notify_event"
+#define GTK_EVENT_TYPE_LEAVE_NOTIFY     "leave_notify_event"
+#define GTK_EVENT_TYPE_CONFIGURE        "configure_event"
+#define GTK_EVENT_TYPE_FOCUS_IN         "focus_in_event"
+#define GTK_EVENT_TYPE_FOCUS_OUT        "focus_out_event"
+#define GTK_EVENT_TYPE_MAC              "map_event"
+#define GTK_EVENT_TYPE_UNMAP            "unmap_event"
+#define GTK_EVENT_TYPE_PROPERTY_NOTIFY  "property_notify_event"
+#define GTK_EVENT_TYPE_SELECTION_CLEAR  "selection_clear_event"
+#define GTK_EVENT_TYPE_SELECTION_REQUEST "selection_request_event"
+#define GTK_EVENT_TYPE_SELECTION_NOTIRY "selection_notify_event"
+#define GTK_EVENT_TYPE_PROXIMITY_IN     "proximity_in_event"
+#define GTK_EVENT_TYPE_PROXIMITY_OUT    "proximity_out_event"
+#define GTK_EVENT_TYPE_VISIBILITY_NOTIFY "visibility_notify_event"
+#define GTK_EVENT_TYPE_CLIENT           "client_event"
+#define GTK_EVENT_TYPE_NO_EXPOSE        "no_expose_event"
+#define GTK_EVENT_TYPE_WINDOW_STATE     "window_state_event"
+
 
 
 
